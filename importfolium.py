9a2784cc8034bafc86a7311e29d6df5fcb934836
bruno <33145112+brunooviedo@users.noreply.github.com>
2022-01-04 04:06:25 -0300

    Update importfolium.py

--- importfolium.py
+++ importfolium.py
@@ -159,6 +159,6 @@ folium.Circle(
 ).add_to(mapa2)
  
         
-mapa1.save('/templates/mapa.html')
-mapa.save('/templates/areas.html')
-mapa2.save('/templates/head.html')
+mapa1.save('templates/mapa.html')
+mapa.save('templates/areas.html')
+mapa2.save('templates/head.html')
